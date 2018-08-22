8b3637f753de1b97612d7faac3a859281a199d29
Volkan Selcuk <vselcuk@gmail.com>
2018-08-22 10:53:34 +0200

    PATHWAY-12 fixed wrong usage of mockito method

--- nifi-flaconi-processors/src/test/java/de/flaconi/nifi/processors/TestConvertJSONToSQL.java
+++ nifi-flaconi-processors/src/test/java/de/flaconi/nifi/processors/TestConvertJSONToSQL.java
@@ -3,12 +3,12 @@ package de.flaconi.nifi.processors;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
-import static org.mockito.Matchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
 import java.sql.Types;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class TestConvertJSONToSQL {
 
@@ -18,7 +18,7 @@ public class TestConvertJSONToSQL {
       ObjectMapper mapper = new ObjectMapper();
       JsonNode node = mapper.readTree(payload);
 
-      String value = ConvertJSONToSQL.createSqlStringValue(node.get("created_at"), anyInt(), Types.TIMESTAMP);
+      String value = ConvertJSONToSQL.createSqlStringValue(node.get("created_at"), 26, Types.TIMESTAMP);
 
       assertThat(value, is("2018-08-17 10:10:10.000"));
     }
